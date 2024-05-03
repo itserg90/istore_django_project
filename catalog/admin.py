1515196f0672b316c154739897830207d0698f7d
Sergei Gorlanov <itserg90@yandex.ru>
2024-05-03 13:21:47 +0300

    v1.1 1)add Category and Product to admin.py 2)add __str__ to Category and Product in models.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Product)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    last_filter = ('category',)
+    search_fields = ('name', 'description')
--- catalog/models.py
+++ catalog/models.py
@@ -5,6 +5,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='имя')
     description = models.TextField(verbose_name='описание')
 
+    def __str__(self):
+        return f'{self.name}: {self.description}'
+
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
@@ -18,6 +21,9 @@ class Product(models.Model):
     price = models.FloatField(verbose_name='цена за покупку')
     created_at = models.DateField(verbose_name='дата создания(записи в БД)')
     updated_at = models.DateField(verbose_name='дата последнего изменения(записи в БД)')
+
+    def __str__(self):
+        return f'({self.name}, {self.description}, {self.image}, {self.price}, {self.created_at}, {self.updated_at})'
 
     class Meta:
         verbose_name = 'продукт'
