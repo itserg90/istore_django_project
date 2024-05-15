9c4cfe38f1a251420b6d3ac7ea4e55237054b32d
Sergei Gorlanov <itserg90@yandex.ru>
2024-05-15 13:33:41 +0300

    v1.5 new model Blog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from catalog.views import ProductsListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from catalog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='product_list'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('product/create/', ProductCreateView.as_view(), name='product_create'),
-    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
-    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
+    path('', BlogListView.as_view(), name='blog_list'),
+    path('product/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('product/create/', BlogCreateView.as_view(), name='blog_create'),
+    path('product/<int:pk>/update/', BlogUpdateView.as_view(), name='blog_update'),
+    path('product/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -2,19 +2,19 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from pytils.translit import slugify
 
-from catalog.models import Product
+from catalog.models import Product, Blog
 
 
-class ProductsListView(ListView):
-    model = Product
+class BlogListView(ListView):
+    model = Blog
 
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(is_published=True)
 
 
-class ProductDetailView(DetailView):
-    model = Product
+class BlogDetailView(DetailView):
+    model = Blog
 
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
@@ -23,29 +23,28 @@ class ProductDetailView(DetailView):
         return self.object
 
 
-class ProductCreateView(CreateView):
-    model = Product
-    fields = ['name', 'category', 'description', 'price', 'image']
-    success_url = reverse_lazy('catalog:product_list')
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ['title', 'content', 'image', 'is_published']
+    success_url = reverse_lazy('catalog:blog_list')
 
     def form_valid(self, form):
         if form.is_valid():
             new_name = form.save()
-            new_name.slug = slugify(new_name.name)
+            new_name.slug = slugify(new_name.title)
             new_name.save()
 
         return super().form_valid(form)
 
 
-class ProductUpdateView(UpdateView):
-    model = Product
-    fields = ['name', 'category', 'description', 'price', 'image']
-    success_url = reverse_lazy('catalog:product_list')
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['title', 'content', 'image', 'is_published']
 
     def get_success_url(self):
-        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
+        return reverse('catalog:blog_detail', args=[self.kwargs.get('pk')])
 
 
-class ProductDeleteView(DeleteView):
-    model = Product
-    success_url = reverse_lazy('catalog:product_list')
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('catalog:blog_list')
